9441e58cd5461567961802f49ea502e885b00d8f
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-10-10 21:49:08 -0300

    muda a paleta de cores e adiciona dinaminsmo

--- gallery.js
+++ gallery.js
@@ -5,26 +5,19 @@ let galleryElement = document.querySelector('.galleryElement');
 let galleryHowMany = projects.childNodes;
 
 
-// Criar projeto piskcel
+// Criar projeto
+function createProject(link,id,url,name) {  
+    let projectLink = document.createElement('a');
+    projectLink.classList.add('galleryElement');
+    projectLink.href = link;
+    projectLink.target = '_blank';
+    projectLink.id = id;
+    projectLink.style.backgroundImage = `url('${url}')`
+    projects.appendChild(projectLink);
+    let createName = document.createElement('p');
+    createName.innerHTML = name;
+    projectLink.appendChild(createName);
+}
 
-let pisqicelDiv = document.createElement('a');
-pisqicelDiv.href = 'https://piedrorockembach.github.io/pisqicel.github.io/'
-pisqicelDiv.target = '_blank';
-pisqicelDiv.id = 'pisqicel';
-pisqicelDiv.classList.add('galleryElement');
-projects.appendChild(pisqicelDiv);
-let pisqicelImage = document.createElement('img');
-pisqicelImage.src = '/images/pisqicel.png';
-pisqicelImage.id = 'pisqicelImg';
-
-pisqicelDiv.appendChild(pisqicelImage);
-
-
-pisqicelImage.style.width = '100%'
-
-gallery.addEventListener("change", function(){
-    gallery.style.width = gallerySize + 'px';
-    
-
-
-});
+//calls
+createProject('https://piedrorockembach.github.io/pisqicel.github.io/','pisqicel','/images/pisqicel.png','Projeto: Pixels-art');
