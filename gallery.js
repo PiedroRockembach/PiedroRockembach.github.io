7a81894c1bf7a76856546f4accb67e4ac6cb6618
piedro <piedrorn@gmail.com>
2023-12-02 23:18:23 -0300

    refatora portfolio

--- gallery.js
+++ gallery.js
@@ -1,23 +1,55 @@
 let gallerySize = (window.screen.availWidth / 100) * 90;
-let gallery = document.querySelector('#gallery');
-let projects = document.querySelector('#projects');
-let galleryElement = document.querySelector('.galleryElement');
+let gallery = document.querySelector('.gallery');
+let projects = document.querySelector('.projects');
+let galleryElement = document.querySelector('.project');
 let galleryHowMany = projects.childNodes;
 
 
 // Criar projeto
-function createProject(link,id,url,name) {  
-    let projectLink = document.createElement('a');
-    projectLink.classList.add('galleryElement');
-    projectLink.href = link;
-    projectLink.target = '_blank';
+function createProject({github, link,id,url,name, description}) {  
+    let projectLink = document.createElement('div');
+    projectLink.classList.add('project');
     projectLink.id = id;
-    projectLink.style.backgroundImage = `url('${url}')`
-    projects.appendChild(projectLink);
-    let createName = document.createElement('p');
+    let projectImg = document.createElement('img');
+    projectImg.src = url;
+    let createName = document.createElement('h2');
     createName.innerHTML = name;
+    let githubLink = document.createElement('a');
+    githubLink.classList.add('githubLink');
+    githubLink.href = github;
+    let descriptionText = document.createElement('p');
+    descriptionText.innerHTML = description;
+    let buttons = document.createElement('div');
+    buttons.classList.add('project-buttons');
     projectLink.appendChild(createName);
+    projectLink.appendChild(projectImg);
+    projectLink.appendChild(descriptionText);
+    buttons.appendChild(githubLink);
+    if(link) {
+        let pageLink = document.createElement('a');
+        pageLink.innerHTML = 'Visitar o site'
+        pageLink.classList.add('project-page-link');
+        pageLink.href = link;
+        buttons.appendChild(pageLink);
+    }
+    projectLink.appendChild(buttons);
+    projects.appendChild(projectLink);
 }
 
 //calls
-createProject('https://piedrorockembach.github.io/pisqicel.github.io/','pisqicel','/images/pisqicel.png','Projeto: Pixels-art');
+createProject({
+    github: 'https://github.com/PiedroRockembach/TrybeTunes-Portifolio',
+    link: 'https://trybe-tunes-delta.vercel.app/',
+    description: 'Um clone do Itunes feito em React consumindo a api do Itunes.',
+    id: 'TrybeTunes',
+    url: './images/TrybeTunes.png',
+    name: 'Trybe Tunes'
+});
+createProject({
+    github: 'https://piedrorockembach.github.io/pisqicel.github.io/',
+    link: 'https://piedrorockembach.github.io/pisqicel.github.io/',
+    description:"Projeto de quadro para pixel art com tamanho personalizavel feito em Html, CSS e JavaScript.",
+    id: 'pisqicel',
+    url:'/images/pisqicel.png',
+    name: 'Pixels-art',
+});
